2a392a715416a6373ca334263cfc40f732766614
aryan <kanadaaryan9@gmail.com>
2024-06-28 12:35:16 +0530

    edited web

--- web.py
+++ web.py
@@ -3,16 +3,18 @@ import functions
 
 todos = functions.get_todos()
 
-
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"]+"\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your creativity")
+st.write("This app is to increase your <b>creativity</b>",
+         unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
